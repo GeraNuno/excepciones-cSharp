f402228352f7b0536a29e58c141cd70a206cdad5
GeraNuno <l21212008@tectijuana.edu.mx>
2022-05-19 11:53:21 -0700

    Excepciones
    Aplicación de excepciones en C#

---
+++ excepciones-cSharp/excepciones-cSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            try
+            {
+                Console.Write("Escribe un número: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.Write("Escribe otro número: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
